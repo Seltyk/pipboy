7c8328bcff4406846a2810bbbd4b1aa13c0a085a
Aayla Semyonova <aayla@aayla.dev>
2021-04-26 18:00:05 -0600

    Started setting up clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+// Copyright (C) 2021 Aayla Semyonova
+// 
+// This file is part of pipboy.
+// 
+// pipboy is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// pipboy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with pipboy.  If not, see <http://www.gnu.org/licenses/>.
+
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    // Load CLI arguments with clap
+    let yaml = load_yaml!("arguments.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
 }
