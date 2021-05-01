57abcfb67444d9cd3d957e94b049a97fd95a9f5c
Aayla Semyonova <aayla@aayla.dev>
2021-05-01 11:52:34 -0600

    Started remote

---
+++ src/remote.rs
@@ -0,0 +1,21 @@
+// Copyright (C) 2021 Aayla Semyonova
+// 
+// This file is part of pipboy.
+// 
+// pipboy is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// pipboy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with pipboy.  If not, see <http://www.gnu.org/licenses/>.
+
+/// Get the package index of a remote repository
+pub(crate) fn get_index(remote: &str) {
+    
+}
